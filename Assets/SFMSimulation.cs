2404ad6a9c08ecbe56fb3babab3f3776832fd2d5
n-shintaro <shintaro0311@keio.jp>
2022-01-29 23:51:09 +0900

    20220129 make sfm_simulation.cs

---
+++ Assets/SFMSimulation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFMSimulation : MonoBehaviour
+{
+    public Transform SFCharacterParent;
+    public Transform WallParent;
+    
+    Wall[] walls;
+    SFCharacter[] agents;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 壁の位置をすべて取得
+       walls=WallParent.GetComponentsInChildren<Wall>();
+       //SFCharcterの位置をすべて取得
+       agents=SFCharacterParent.GetComponentsInChildren<SFCharacter>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
